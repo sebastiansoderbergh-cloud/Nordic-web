2dbf31d2f2d7d639e6976e90e1d402478c988a67
sebastiansoderbergh-cloud <sebastian.soderbergh@gmail.com>
2026-08-22 13:38:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function gaTillKontakt() {
+    document.getElementById("kontakt").scrollIntoView();
+}
+function gaTillKontakt() {
+    document.getElementById("kontakt").scrollIntoView({
+        behavior: "smooth"
+    });
+}
