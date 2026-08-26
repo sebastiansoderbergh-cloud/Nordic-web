d1eb18638617d64c4c51e96b059204cf437d0b01
sebastiansoderbergh-cloud <sebastian.soderbergh@gmail.com>
2026-08-26 15:28:05 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function gaTillKontakt() {
         behavior: "smooth"
     });
 }
+const form = document.getElementById("contact-form");
+const successMessage = document.getElementById("success-message");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    successMessage.style.display = "block";
+    form.reset();
+});
+const bookingForm = document.getElementById("booking-form");
+const bookingMessage = document.getElementById("booking-message");
+
+if (bookingForm) {
+    bookingForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        bookingMessage.style.display = "block";
+        bookingForm.reset();
+    });
+}
